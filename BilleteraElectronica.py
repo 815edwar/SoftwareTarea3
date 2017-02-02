ffd46b4859ba91f7e48c0f228a31b1a6086fd3fb
Edwar Yepez <edwarypz@gmail.com>
2017-02-01 21:34:34 -0500

    Script .py created

---
+++ BilleteraElectronica.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Authors:	Edwar Yepez		Carnet: 12-10855
+# 			Edgar Silva		Carnet: 
+# 
+
+from datetime import *
+import datetime
+
+# Billetera Electronica
+# 
